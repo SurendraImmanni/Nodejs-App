dd25ab328064462847a770901e356c9ed317cd20
Surendraimmani <surendraimmani@gmail.com>
2024-11-29 20:30:16 +0530

    created a test file to run tests

---
+++ webapp/test/app.test.js
@@ -0,0 +1,12 @@
+import request from "supertest"; // Import supertest
+import app from "../app"; // Import the app (export your express app from the main file)
+
+describe("GET /", () => {
+  it("should return a 200 status and the correct message", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Welcome to DevOps World");
+  });
+});
+
