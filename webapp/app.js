718f5f553e3ea388798797ab43a501e464854b1f
Surendraimmani <surendraimmani@gmail.com>
2024-11-30 23:33:12 +0530

    chnages

--- webapp/app.js
+++ webapp/app.js
@@ -1,19 +1,16 @@
 import express from "express";
 
-const app=express()
-const port=3000
+const app = express();
+const port = 3000;
 
-app.get("/",(req, res)=>{
-    res.send("Welcome to DevOps World");
-
+app.get("/", (req, res) => {
+  res.send("Welcome to DevOps World");
 });
 
-// Export the app for testing purposes
-export default app;
 
-// Only start the server if this file is directly executed (not during tests)
-if (import.meta.url === new URL(import.meta.url).href) {
-app.listen(port, ()=>{
-    console.log( `Application is running in ${port}` )
-})
-}
+const server = app.listen(port, () => {
+  console.log(`Application is running in ${port}`);
+});
+
+export { app, server };  // Export the app for testing
+
